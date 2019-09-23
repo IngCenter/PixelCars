a1b50fd81095ed7440f92a0fd9494a39fc5ebd26
CAHIH <55281316+CAHIH@users.noreply.github.com>
2019-09-23 15:08:51 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
-
 #include "TXLib.h"
+
+struct Knopka
+{
+     int x;
+     int y;
+     const char* text;
+
+};
+
+void drawButton(Knopka knop)
+{
+
+    txSetFillColor(TX_LIGHTBLUE);
+    txRectangle(knop.x + 10,knop.y + 10,knop.x + 100,knop.y + 50);
+    txDrawText (knop.x + 10,knop.y + 10,knop.x + 100,knop.y + 50, knop.text);
+
+}
+
 void drawButton(int x , int y, const char* text)
 {
     txSetFillColor(TX_LIGHTBLUE);
@@ -7,6 +24,21 @@ void drawButton(int x , int y, const char* text)
     txDrawText (x + 10,y + 10,x + 100,y + 50, text);
 
 }
+
+bool Click (Knopka knop)
+{
+ if(txMouseButtons() == 1 &&
+    txMouseX() > knop.x + 10 &&
+    txMouseX() < knop.x + 100 &&
+    txMouseY() > knop.y + 10 &&
+    txMouseY() < knop.y + 50)
+ {
+  return true;
+ }
+
+  return false;
+}
+
 
 bool Click (int x, int y)
 {
@@ -26,90 +58,37 @@ int main()
 {
     txCreateWindow (1200, 600);
 
-HDC image = txLoadImage ("apple.bmp");
+    HDC image = txLoadImage ("apple.bmp");
+
+    Knopka knop[10];
+    knop[0] = {0, 0,   "Кузов"};
+    knop[1] = {100, 10, "Колеса"};
+    knop[2] = {200, 0, "Спойлер"};
+    knop[3] = {300,10, "Крыша"};
+    knop[4] = {400,0,  "Выхлоп"};
+    knop[5] = {500,10, "Танировка"};
+    knop[6] = {600,0,  "Двигатель"};
+    knop[7] = {700,10, "Крылья П."};
+    knop[8] = {800,0, "Крылья З."};
+    knop[9] = {900,10, "Пороги"};
 
     while (true)
     {
-
         txBegin();
-
         txSetFillColor(TX_WHITE);
-
         txClear();
-
         txSetColor(TX_PINK);
 
-        drawButton(0, 0,   "Кузов");
-
-        if (Click(0, 0))
+        for (int nomer = 0; nomer < 10; nomer = nomer + 1)
         {
-            txTextOut(400, 10, "ты лох");
+            drawButton(knop[nomer]);
+            if (Click(knop[nomer]))
+            {
+                txTextOut(knop[nomer].x, knop[nomer].y + 100, "ты лох");
+            }
         }
 
-        drawButton(100,10, "Колеса");
-
-        if (Click(100, 10))
-        {
-            txTextOut(500, 10, "ты лох");
-        }
-
-        drawButton(200,0,  "Спойлер");
-
-        if (Click(200, 0))
-        {
-            txTextOut(600, 10, "ты лох");
-        }
-
-        drawButton(300,10, "Крыша");
-
-        if (Click(300, 10))
-        {
-            txTextOut(700, 10, "ты лох");
-        }
-
-        drawButton(400,0,  "Выхлоп");
-
-        if (Click(400, 0))
-        {
-            txTextOut(800, 10, "ты лох");
-        }
-
-        drawButton(500,10, "Танировка");
-
-        if (Click(500, 10))
-        {
-            txTextOut(900, 10, "ты лох");
-        }
-
-        drawButton(600,0,  "Двигатель");
-
-        if (Click(600, 0))
-        {
-            txTextOut(1000, 10, "ты лох");
-        }
-
-        drawButton(700,10, "Крылья П.");
-
-        if (Click(700, 10))
-        {
-            txTextOut(1000, 10, "ты лох");
-        }
-
-        drawButton(800,0,  "Крылья З.");
-
-        if (Click(800, 0))
-        {
-            txTextOut(1000, 10, "ты лох");
-        }
-
-        drawButton(900,10, "Пороги");
-
-        if (Click(900, 10))
-        {
-            txTextOut(1000, 10, "ты лох");
-        }
-
-
+        /*
         txSetFillColor(TX_BLUE);
         txSetColor(TX_PINK);
         txRectangle(100,100,825,525);
@@ -126,7 +105,7 @@ HDC image = txLoadImage ("apple.bmp");
         Win32::TransparentBlt (txDC(), 1110,180, 70, 70, image, 0, 0, 40, 40, TX_WHITE);
         Win32::TransparentBlt (txDC(), 1030,260, 70, 70, image, 0, 0, 40, 40, TX_WHITE);
         Win32::TransparentBlt (txDC(), 1110,260, 70, 70, image, 0, 0, 40, 40, TX_WHITE);
-
+         */
 
 
         txSleep(10);
