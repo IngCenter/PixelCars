81bd6e0510219fc67e533456fbb5db3e7a6bebc3
DimaPopkov <55281177+DimaPopkov@users.noreply.github.com>
2019-10-11 15:31:02 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -19,25 +19,27 @@ int main()
     knop[9] = {900,10, "Пороги"};
 
     char* category;
+    char* category2;
+    int COUNT_PICS = 12;
 
-    MapObject pic[15];
+    MapObject pic[COUNT_PICS];
     pic[0] = {1030,80,140,70,  txLoadImage ("Pics\\Car1.bmp"),  true,486,138,"Car"};
     pic[1] = {1030,160,140,70, txLoadImage ("Pics\\Car2.bmp"),  true,363,113,"Car"};
-    pic[2] = {1030,240,140,70,  txLoadImage ("Pics\\Car3.bmp"),  true,525,151,"Car"};
-    pic[3] = {1030,320,140,70,  txLoadImage ("Pics\\Car4.bmp"),  true,457,135,"Car"};
-    pic[4] = {1030,400,140,70,  txLoadImage ("Pics\\Car5.bmp"),  true,311,89,"Car"};
+    pic[2] = {1030,240,140,70, txLoadImage ("Pics\\Car3.bmp"),  true,525,151,"Car"};
+    pic[3] = {1030,320,140,70, txLoadImage ("Pics\\Car4.bmp"),  true,457,135,"Car"};
+    pic[4] = {1030,400,140,70, txLoadImage ("Pics\\Car5.bmp"),  true,311,89,"Car"};
     pic[5] = {1030,480,140,70, txLoadImage ("Pics\\Car6.bmp"),  true,311,89,"Car"};
-    pic[6] = {1030,160,70,70,  txLoadImage ("Pics\\Wheel2.bmp"),true,21,21,"Wheel"};
-    pic[7] = {1110,160,70,70,  txLoadImage ("Pics\\Wheel2.bmp"),true,21,21,"Wheel"};
-    pic[8] = {1030,240,70,70,  txLoadImage ("Pics\\Wheel3.bmp"),true,21,21,"Wheel"};
-    pic[9] = {1110,240,70,70,  txLoadImage ("Pics\\Wheel3.bmp"),true,21,21,"Wheel"};
-    pic[10] ={1030,80,70,70,   txLoadImage ("Pics\\Spoler.bmp"),true,40,40,"Spoler"};
-    pic[11] ={1030,200,70,30,   txLoadImage ("Pics\\Spoler2Vozvrachenie.bmp"),true,25,8,"Spoler"};
+    pic[6] = {1030,160,70,70,  txLoadImage ("Pics\\Wheel2.bmp"),true,21,21,"WheelLeft"};
+    pic[7] = {1110,160,70,70,  txLoadImage ("Pics\\Wheel2.bmp"),true,21,21,"WheelRight"};
+    pic[8] = {1030,240,70,70,  txLoadImage ("Pics\\Wheel3.bmp"),true,21,21,"WheelLeft"};
+    pic[9] = {1110,240,70,70,  txLoadImage ("Pics\\Wheel3.bmp"),true,21,21,"WheelRight"};
+    pic[10]= {1030,80,70,70,   txLoadImage ("Pics\\Spoler.bmp"),true,40,40,"Spoler"};
+    pic[11]= {1030,200,70,30,  txLoadImage ("Pics\\Spoler2.bmp"),true,25,8,"Spoler"};
 
 
-    MapObject mapParts[15];
+    MapObject mapParts[COUNT_PICS];
     mapParts[0] =  {205,380,486,138,txLoadImage ("Pics\\Car1.bmp")  ,false,486,138,""};
-    mapParts[1] =  {205,180,363,113,txLoadImage ("Pics\\Car2.bmp")  ,false,363,113,""};
+    mapParts[1] =  {190,370,510,155,txLoadImage ("Pics\\Car2.bmp")  ,false,363,113,""};
     mapParts[2] =  {225,378,450,140,txLoadImage ("Pics\\Car3.bmp")  ,false,525,151,""};
     mapParts[3] =  {600,351,457,135,txLoadImage ("Pics\\Car4.bmp")  ,false,457,135,""};
     mapParts[4] =  {600,351,311,89, txLoadImage ("Pics\\Car5.bmp")  ,false,311,89,""};
@@ -46,8 +48,8 @@ int main()
     mapParts[7] =  {560,450,63,63,  txLoadImage ("Pics\\Wheel2.bmp"),false,21,21,""};
     mapParts[8] =  {280,450,63,63,  txLoadImage ("Pics\\Wheel3.bmp"),false,21,21,""};
     mapParts[9] =  {560,450,63,63,  txLoadImage ("Pics\\Wheel3.bmp"),false,21,21,""};
-    mapParts[10] = {600,353,100,100,txLoadImage ("Pics\\Spoler.bmp"),false,40,40,""};
-    mapParts[11] ={600,353,100,100,   txLoadImage ("Pics\\Spoler2Vozvrachenie.bmp"),false,40,40,""};
+    mapParts[10]=  {600,353,100,100,txLoadImage ("Pics\\Spoler.bmp"),false,40,40,""};
+    mapParts[11]=  {600,353,100,100,txLoadImage ("Pics\\Spoler2.bmp"),false,25,8,""};
 
 
 
@@ -74,35 +76,37 @@ int main()
         txSetColor(TX_RED);
         txRectangle(1020,10,1190,590);
 
-        if (GetAsyncKeyState('A'))
+        for (int nomer = 0; nomer < COUNT_PICS; nomer = nomer + 1)
         {
-            pic[0].visible = !pic[0].visible;
-        }
-        if (GetAsyncKeyState('S'))
-        {
-            pic[1].visible = !pic[1].visible;
-        }
-
-        for (int nomer = 0; nomer < 12; nomer = nomer + 1)
-        {
-            if (pic[nomer].category == category)
+            if (pic[nomer].category == category || pic[nomer].category == category2)
             {
-            drawPicture2 (pic[nomer]);
+                drawPicture2 (pic[nomer]);
             }
             drawPicture2 (mapParts[nomer]);
         }
 
 
-        for (int a = 0; a < 12; a++)
+        for (int a = 0; a < COUNT_PICS; a++)
         {
             if (txMouseButtons() == 1 &&
                 txMouseX() > pic[a].x &&
                 txMouseX() < pic[a].x + pic[a].shirina &&
                 txMouseY() > pic[a].y &&
                 txMouseY() < pic[a].y + pic[a].visota &&
-                pic[a].category == category)
+                (pic[a].category == category || pic[a].category == category2))
             {
                 mapParts[a].visible = !mapParts[a].visible;
+
+                for (int n = 0; n < COUNT_PICS; n++)
+                {
+                    if (mapParts[a].visible &&
+                        a != n &&
+                        pic[n].category == pic[a].category)
+                    {
+                        mapParts[n].visible = false;
+                    }
+                }
+
                 txSleep(200);
             }
         }
@@ -113,9 +117,10 @@ int main()
             txSleep(200);
         }
 
-       if (click(knop[1]))
+        if (click(knop[1]))
         {
-            category = "Wheel";
+            category = "WheelLeft";
+            category2 = "WheelRight";
             txSleep(200);
         }
 
@@ -125,9 +130,15 @@ int main()
             txSleep(200);
         }
 
-      txSleep(10);
+        txSleep(10);
         txEnd();
 
+    }
+
+    for (int nomer = 0; nomer < COUNT_PICS; nomer++)
+    {
+        txDeleteDC(pic[nomer].image);
+        txDeleteDC(mapParts[nomer].image);
     }
 
     return 0;
